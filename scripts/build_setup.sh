e459bfe73b619f47c29f5afea7d4bb5538b6c378
Gregory Beaver <greg@unlock-protocol.com>
2019-01-08 14:53:14 -0500

    this runs from the parent directory

--- scripts/build_setup.sh
+++ scripts/build_setup.sh
@@ -3,7 +3,7 @@
 # this build script calls other scripts in sequence
 # in order to simplify travis.yml
 
-./pull-image.sh
-./build-image.sh unlock 461040265176.dkr.ecr.us-east-1.amazonaws.com/unlock:latest
-./build-image.sh unlock-integration
-./docker-compose-build.sh
+scripts/pull-image.sh
+scripts/build-image.sh unlock 461040265176.dkr.ecr.us-east-1.amazonaws.com/unlock:latest
+scripts/build-image.sh unlock-integration
+scripts/docker-compose-build.sh
