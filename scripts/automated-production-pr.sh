6fa669d3454725ee3759197353bd3c99b9ff5d08
Julien Genestoux <julien.genestoux@gmail.com>
2024-05-30 15:43:11 -0400

    more

--- scripts/automated-production-pr.sh
+++ scripts/automated-production-pr.sh
@@ -14,31 +14,21 @@ echo "Creating a new branch"
 BRANCH="production-$(date +%Y%m%d-%H%M%S)"
 git checkout -b $BRANCH $COMMIT_TO_DEPLOY
 
-
-echo ""
-echo "________"
-echo "BRANCHES"
-git branch -a
-echo ""
-echo ""
-echo ""
-echo ""
-
 echo "Diffing versus latest production"
 LATEST_PRODUCTION=`git rev-parse origin/production`
 git reset --soft $LATEST_PRODUCTION
 
-echo "Committing diff"
-COMMIT_MESSAGE="Manual deploy as of commit:$COMMIT_TO_DEPLOY"
-git commit -m "$COMMIT_MESSAGE" -a --no-verify
+# echo "Committing diff"
+# COMMIT_MESSAGE="Manual deploy as of commit:$COMMIT_TO_DEPLOY"
+# git commit -m "$COMMIT_MESSAGE" -a --no-verify
 
-echo "Push new production branch"
-git push origin $BRANCH --no-verify
+# echo "Push new production branch"
+# git push origin $BRANCH --no-verify
 
-echo "Open pull request"
-PROD_DEPLOY_TYPE="manual"
-source "${BASH_SOURCE%/*}/production-pull-request-template.sh"
+# echo "Open pull request"
+# PROD_DEPLOY_TYPE="manual"
+# source "${BASH_SOURCE%/*}/production-pull-request-template.sh"
 
-gh pr create --head $BRANCH --title "Production Deploy" --base production --body "$MESSAGE"
+# gh pr create --head $BRANCH --title "Production Deploy" --base production --body "$MESSAGE"
 
-git checkout master
+# git checkout master
