a27ddb838fb263369109c25748f9aa17b669f75a
julien51 <julien51@users.noreply.github.com>
2024-05-30 20:08:50 +0000

    production release as of hahah

--- scripts/automated-production-pr.sh
+++ scripts/automated-production-pr.sh
@@ -2,55 +2,29 @@
 
 set -e
 
-# Pull all remote
-git fetch origin
-
-# get the timestamp of the latests commit to be deployed
-COMMIT_TO_DEPLOY=`git rev-list -1 --before={2.days.ago} master`
+COMMIT_TO_DEPLOY=$GITHUB_SHA
 COMMIT_TO_DEPLOY_TIMESTAMP=`git show -s --format=%ct $COMMIT_TO_DEPLOY`
 
-# Get the timestamp of the latest commit deployed
-# For this we need to parse the commit message of the latest production commit
-# IMPORTANT: all of our commits on production need to have the latest commit from master which they deploy
-# using the following format: commit:<SHA1>.
-# Automated deploys will not work if we are not able to get that commit.
+echo "This command will open a pull request to deploy $COMMIT_TO_DEPLOY to production"
 
-LATEST_PRODUCTION_COMMIT_MESSAGE=`git log -1 origin/production --pretty=%B`
-COMMIT_REGEX="commit:([0-9a-f]{40})"
+echo "Syncing branches"
+git fetch
 
-if [[ $LATEST_PRODUCTION_COMMIT_MESSAGE =~ $COMMIT_REGEX ]]
-then
-  LATEST_COMMIT_ID_IN_PRODUCTION="${BASH_REMATCH[1]}"
-else
-  echo "Skipping automated deployment. Latest production does not include commit sha1 (this is to avoid deploying an older version of master)."
-  exit 0
-fi
-
-LATEST_PRODUCTION_TIMESTAMP=`git show -s --format=%ct $LATEST_COMMIT_ID_IN_PRODUCTION`
-
-if [[ $((COMMIT_TO_DEPLOY_TIMESTAMP)) < $((LATEST_PRODUCTION_TIMESTAMP)) ]]
-then
-  COMMIT_TO_DEPLOY_DATE=`date -d @$COMMIT_TO_DEPLOY_TIMESTAMP`
-  LATEST_PRODUCTION_DATE=`date -d @$LATEST_PRODUCTION_TIMESTAMP`
-  echo "Latest production ($LATEST_PRODUCTION: $LATEST_PRODUCTION_DATE) is more recent than stable master ($COMMIT_TO_DEPLOY: $COMMIT_TO_DEPLOY_DATE) : skipping deployment."
-  exit 0
-fi
-
-COMMIT_MESSAGE="Automated deploy as of commit:$COMMIT_TO_DEPLOY"
-
-BRANCH="production-$(date +%Y%m%d-%H%M%S)"
-
-DOY=$(date +%j)
-declare -a REVIEWERS
-REVIEWERS=( julien51 clemsos searchableguy kalidiagne )
-REVIEWER_INDEX=$(($DOY % ${#REVIEWERS[@]}))
-REVIEWER=${REVIEWERS[$REVIEWER_INDEX]}
-
-echo '["'$COMMIT_MESSAGE'","'$BRANCH'","'$REVIEWER'"]'
-
-# Checking master out at COMMIT_TO_DEPLOY
-git checkout -b $BRANCH $COMMIT_TO_DEPLOY
-
-# Diffing versus latest production
+echo "Diffing versus latest production"
 LATEST_PRODUCTION=`git rev-parse origin/production`
 git reset --soft $LATEST_PRODUCTION
+
+# echo "Committing diff"
+# COMMIT_MESSAGE="Manual deploy as of commit:$COMMIT_TO_DEPLOY"
+# git commit -m "$COMMIT_MESSAGE" -a --no-verify
+
+# echo "Push new production branch"
+# git push origin $BRANCH --no-verify
+
+# echo "Open pull request"
+# PROD_DEPLOY_TYPE="manual"
+# source "${BASH_SOURCE%/*}/production-pull-request-template.sh"
+
+# gh pr create --head $BRANCH --title "Production Deploy" --base production --body "$MESSAGE"
+
+# git checkout master
